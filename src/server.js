6976b6db3ea0e51b115f3fef660012a657e9a78e
pkro <pkrombach@web.de>
2020-04-18 19:36:30 +0200

    added body-parser

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 const PORT = 8000;
 
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send(`you sent ${req.body.key}`);
 });
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
