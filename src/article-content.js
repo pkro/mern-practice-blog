436715b3d23475f2f369922d7d9f661f9b1e9651
pkro <pkrombach@web.de>
2020-04-18 19:59:16 +0200

    serve and upvote articles

---
+++ src/article-content.js
@@ -0,0 +1,20 @@
+const articles = [
+  {
+    name: 'yay-article',
+    title: 'yay article',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro soluta obcaecati sint molestiae repellendus numquam vel nemo officia quibusdam, ad corrupti perferendis rerum laborum eaque quae qui maiores.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro soluta obcaecati sint molestiae repellendus numquam vel nemo officia quibusdam, ad corrupti perferendis rerum laborum eaque quae qui maiores.',
+    ],
+  },
+  {
+    name: 'yay-article2',
+    title: 'yay another article',
+    content: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro soluta obcaecati sint molestiae repellendus numquam vel nemo officia quibusdam, ad corrupti perferendis rerum laborum eaque quae qui maiores.',
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione optio porro soluta obcaecati sint molestiae repellendus numquam vel nemo officia quibusdam, ad corrupti perferendis rerum laborum eaque quae qui maiores.',
+    ],
+  },
+];
+
+export default articles;
--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import articles from './article-content';
 
 const app = express();
 const PORT = 8000;
 
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+
+const articlesInfo = {
+  'yay-article': {
+    upvotes: 0,
+  },
+  'yay-article2': {
+    upvotes: 0,
+  },
+};
+app.get('/api/articles/:name', (req, res) => {
+  res.send(articles.find((article) => article.name === req.params.name));
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send(`you sent ${req.body.key}`);
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const articleName = req.params.name;
+  if (articleName in articlesInfo) {
+    articlesInfo[articleName].upvotes++;
+    return res
+      .status(200)
+      .json({ [articleName]: articlesInfo[articleName].upvotes });
+  }
+  return res.status(404).send('Article not found');
 });
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
